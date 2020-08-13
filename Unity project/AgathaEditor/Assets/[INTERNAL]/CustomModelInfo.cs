53afed91be45ab63e63dfc4d35389d792d36c411
Sebastián <amarillo.sebas@gmail.com>
2020-08-13 01:41:21 -0500

    Project setup
    -Added internal scripts

---
+++ Unity project/AgathaEditor/Assets/[INTERNAL]/CustomModelInfo.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CustomModelInfo : MonoBehaviour {
+	public ModelType modelType;
+	public SkinnedMeshRenderer blendshapesRenderer;
+	public Transform jawBone;
+	public Transform[] eyes;
+	public Transform[] eyeLids;
+}
+
+public enum ModelType {
+	Vroid,
+	AnimatedMouth,
+	NoMouth
+}
